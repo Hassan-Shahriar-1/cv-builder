2e16926760e62fd5225ba330d117b58c87ae2179
Hassan Shahriar <41904536+Hassan-Shahriar-1@users.noreply.github.com>
2023-04-28 20:02:21 +0600

    default user role setup in UserService

---
+++ app/Services/UserService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services;
+
+class userService 
+{
+
+    //default user role
+    const USER_ROLE = '32a7370e-4d8d-41ac-90ea-cadf1d3c2cb4';
+    //default admin Role
+    const ADMIN_ROLE = 'd588c0ff-18a1-4435-8aee-1bf2c0b8179a';
+}
