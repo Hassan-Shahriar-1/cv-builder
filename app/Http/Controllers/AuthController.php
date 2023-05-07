083b6af948028fe23ac8af69c8edc1eca629569b
Hassan Shahriar <41904536+Hassan-Shahriar-1@users.noreply.github.com>
2023-05-07 11:35:55 +0600

    CB-4: complete login process

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponseHelper;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use App\Http\Resources\UserResource;
 use App\Services\userService;
 use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -33,6 +35,40 @@ class AuthController extends Controller
             return ApiResponseHelper::otherResponse(true, 200, 'created', $user, 201);
         } catch (Exception $e) {
             DB::rollBack();
+            return ApiResponseHelper::serverError($e);
+        }
+    }
+
+    /**
+     * Login
+     * @param
+     * @return JsonResponse
+     */
+    public function login(LoginRequest $request) :JsonResponse
+    {
+        $credentials = $request->only('email', 'password');
+        $status = false;
+        $code = 400;
+        try{
+            $user = User::where('email', $request->email)->first();
+            if ($user) {
+                if (Hash::check($request->password, $user->password)) {
+                    $msg = '';
+                    $status = true;
+                    $code = 200;
+                    $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                    $data = ['token' => $token];
+               
+                } else {
+                    $msg = 'Password mismatch';
+                    $data = [];
+                }
+            } else {
+                $msg = 'User does not exist';
+                $data = [];
+            }
+            return ApiResponseHelper::otherResponse($status, $code, $msg, $data, 200);
+        } catch (Exception $e) {
             return ApiResponseHelper::serverError($e);
         }
     }
