c62497477b4a3032518a7e33da158995ac277e8f
Hassan Shahriar <41904536+Hassan-Shahriar-1@users.noreply.github.com>
2023-05-10 14:58:28 +0600

    User service bug fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,9 +17,10 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function __construct(private userService $userService)
+    private $userService;
+    public function __construct()
     {
-        
+        $this->userService = new userService();
     }
     /**
      * User Registration
